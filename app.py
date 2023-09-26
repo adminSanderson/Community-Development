d19fb786da3d258d65c3db7a2da617c4019f51fd
Alex <99817927+adminSanderson@users.noreply.github.com>
2023-09-26 12:05:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,3 +72,28 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
+@echo off
+echo Добро пожаловать в мою программу на языке cmd!
+echo.
+echo Текущая дата и время:
+echo.
+date /t
+time /t
+echo.
+echo Выключить удаленный компьютер? (y/n)
+set /p choice=
+if "%choice%"=="y" (
+    echo.
+    echo Запуск команды для выключения удаленного компьютера...
+    start /B shutdown /m \\ИМЯ_КОМПЬЮТЕРА /s /f /t 0
+) else (
+    echo.
+    echo Выбрано не выключать компьютер.
+)
+echo.
+echo Спасибо за использование программы!
+pause
+
